7ae058b022b91b93724a9f84ddf672339c62dc62
Devon Weldon <deveathbo@gmail.com>
2017-04-18 21:11:02 -0700

    Added middleware to serve up static files

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const PORT = 1337;
 const IP = '127.0.0.1';
 const app = express();
 
-//TODO serve up index.htm,l on GET /
-
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/client/index.html');
 });
+
+//serve static assests
+app.use('/client', express.static(__dirname + '/client'));
+
 
 app.listen(PORT, () => {
   console.log(`learn-you-hangukaml express edition is listening on port ${PORT}`);
